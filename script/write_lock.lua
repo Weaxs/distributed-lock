e63dc8d765f18f5b3cbdff7db9de5bc0c30cf590
Weaxs <459312872@qq.com>
2022-08-12 01:35:07 +0800

    write lock

--- script/write_lock.lua
+++ script/write_lock.lua
@@ -32,11 +32,27 @@ function unlock(KEYS, ARGV)
         return 1;
     end;
     if mode == 'write' then
-        local lockExists = redis.call('hexists', KEYS[1], ARGV[2])
+        local writeThreadKey =  ARGV[2] .. ':write'
+        local lockExists = redis.call('hexists', KEYS[1], writeThreadKey)
         if lockExists == 0 then
-             return nil;
-        end
-
+            return nil;
+        else
+            local counter = redis.call('hincrby', KEYS[1], writeThreadKey, -1);
+            if counter > 0 then
+                redis.call('pexpire', KEYS[1], ARGV[1]);
+                return 0;
+            else
+                redis.call('hdel', KEYS[1], writeThreadKey);
+                -- only remain mode
+                if (redis.call('hlen', KEYS[1]) == 1) then
+                    redis.call('del', KEYS[1]);
+                else
+                    -- has unlocked self read-lock
+                    redis.call('hset', KEYS[1], 'mode', 'read');
+                end;
+                return 1;
+            end;
+        end;
     end;
     return nil;
 end
