e7d9f9d14fb5c389409552b052270bd914c8ec0a
Weaxs <459312872@qq.com>
2022-09-08 00:33:15 +0800

    read write lock

--- redis/writelock_test.go
+++ redis/writelock_test.go
@@ -25,3 +25,31 @@ func TestWriteLock(t *testing.T) {
 	_ = writeLock.Unlock()
 
 }
+
+func TestName(t *testing.T) {
+	option := &redis.Options{
+		Addr:     "localhost:49153",
+		Username: "default",
+		Password: "redispw",
+	}
+	rdb := redis.NewClient(option)
+
+	key := "read-write-lock"
+	readLock := NewReadLock(rdb, key)
+	writeLock := NewWriteLock(rdb, key)
+	writeLock.id = readLock.id
+	_, _ = writeLock.LockWithTime(defaultTestExpire, defaultTestTimeout)
+	_, _ = readLock.LockWithTime(defaultTestExpire, defaultTestTimeout)
+	mode := rdb.HGet(context.Background(), key, "mode").Val()
+	assert.Equal(t, "write", mode)
+	hLen := rdb.HLen(context.Background(), key).Val()
+	assert.Equal(t, int64(3), hLen)
+
+	_ = writeLock.Unlock()
+	mode = rdb.HGet(context.Background(), key, "mode").Val()
+	assert.Equal(t, "read", mode)
+	hLen = rdb.HLen(context.Background(), key).Val()
+	assert.Equal(t, int64(2), hLen)
+
+	_ = readLock.Unlock()
+}
